67d7cdc35201fccc3caafeecccc3d90f19b99d94
Masterwood <kimura.unit@gmail.com>
2016-07-27 19:59:19 +0900

    フッターエリアをphpのインクルード導入

---
+++ div/footer/footer.php
@@ -0,0 +1,13 @@
+<div class="footer">
+  <p class="fs_14">Dreams of HYPER AMBITION are go to space by business growth.</p>
+</div>
+<div class="footer_list">
+  <ul>
+    <li><a href="../index.html">トップページ</a></li>
+    <li><a href="../business/index.html">サービス紹介</a></li>
+    <li><a href="">運用メディア</a></li>
+    <li><a href="">事業情報</a></li>
+    <li><a href="">お問い合わせ</a></li>
+  </ul>
+  <p class="fs_14 clear_B txt_C">Copyright© 2016 HYPER AMBITION運営者   All Rights Reserved.</p>
+</div>
